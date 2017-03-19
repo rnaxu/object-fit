a4fc58668c1bfb04372d5eca9b0e9d055e3d4bd3
rnaxu <luv.s2.rnaxu@gmail.com>
2017-03-19 23:43:48 +0900

    fix indent

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
 /* global module, __dirname */
 
 module.exports = {
-    entry: './src/js/entry.js',
-    output: {
-        path: __dirname,
-        filename: 'bundle.js'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.scss/,
-          loaders: ["style-loader", "css-loader", "sass-loader"]
-        },
-      ]
-    }
+  entry: './src/js/entry.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      },
+    ]
+  }
 };
